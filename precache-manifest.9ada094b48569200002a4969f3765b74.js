46640a85d39cf8edbd4d27ecf24d03254bd3a84e
root <alet.aurelien@gmail.com>
2019-01-08 23:08:27 +0100

    Updates

---
+++ precache-manifest.9ada094b48569200002a4969f3765b74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eabc0d1ee5d2133b1901",
+    "url": "/products-app/static/js/runtime~main.eabc0d1e.js"
+  },
+  {
+    "revision": "1478f86625a8c831ea80",
+    "url": "/products-app/static/js/main.1478f866.chunk.js"
+  },
+  {
+    "revision": "97c690e4b8cebefe7f02",
+    "url": "/products-app/static/js/1.97c690e4.chunk.js"
+  },
+  {
+    "revision": "1478f86625a8c831ea80",
+    "url": "/products-app/static/css/main.4a334ce0.chunk.css"
+  },
+  {
+    "revision": "e71203e514405aaf922b1fc822b18d27",
+    "url": "/products-app/index.html"
+  }
+];
